5505675b0426eef58899dc1f08c66ba0de787f7c
Geert van Horrik <geert@catenalogic.com>
2019-11-12 20:28:17 +0100

    Add sorted and regular dictionary benchmarks

--- PropertyBagResearch/Benchmarks/SortedDictionaryBenchmark.cs
+++ PropertyBagResearch/Benchmarks/SortedDictionaryBenchmark.cs
@@ -7,5 +7,37 @@
 
     public class SortedDictionaryBenchmark : BenchmarkBase
     {
+        private TestType _regularDictionary = new TestType(new NonTypedPropertyBag(new DictionaryFactory()));
+        private TestType _sortedDictionary = new TestType(new NonTypedPropertyBag(new DictionaryFactory()));
+
+        public SortedDictionaryBenchmark()
+        {
+            _regularDictionary.BoolValue = true;
+            _regularDictionary.ShortValue = 42;
+            _regularDictionary.IntValue = 42;
+            _regularDictionary.LongValue = 42l;
+
+            _sortedDictionary.BoolValue = true;
+            _sortedDictionary.ShortValue = 42;
+            _sortedDictionary.IntValue = 42;
+            _sortedDictionary.LongValue = 42l;
+        }
+
+        [Benchmark]
+        public int SortedDictionary()
+        {
+            return GetInt(_sortedDictionary);
+        }
+
+        [Benchmark]
+        public int RegularDictionary()
+        {
+            return GetInt(_regularDictionary);
+        }
+
+        public int GetInt(TestType testType)
+        {
+            return testType.IntValue;
+        }
     }
 }
